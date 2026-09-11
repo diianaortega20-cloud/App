20a11027c1894d21980a96753e0311cee1104990
diianaortega20-cloud <diiana.ortega20@gmail.com>
2026-09-11 09:42:29 -0600

    firsr commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,66 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonContent } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonContent,
+    FormsModule,
+    CommonModule
+  ],
 })
 export class Tab1Page {
+
+  username: string = '';
+  password: string = '';
+
+  usernameFocused: boolean = false;
+  passwordFocused: boolean = false;
+
+  isLoginAnimating: boolean = false;
+  isLoginMoving: boolean = false;
+
+  authenticating: boolean = false;
+  authReturning: boolean = false;
+
+  hideLoginContent: boolean = false;
+  loginSuccess: boolean = false;
+
   constructor() {}
+
+  login(): void {
+
+    if (this.isLoginAnimating) {
+      return;
+    }
+
+    this.isLoginAnimating = true;
+
+    setTimeout(() => {
+      this.isLoginMoving = true;
+    }, 300);
+
+    setTimeout(() => {
+      this.authenticating = true;
+    }, 500);
+
+    setTimeout(() => {
+      this.authReturning = true;
+      this.authenticating = false;
+      this.isLoginMoving = false;
+    }, 2500);
+
+    setTimeout(() => {
+      this.isLoginAnimating = false;
+      this.hideLoginContent = true;
+    }, 2800);
+
+    setTimeout(() => {
+      this.loginSuccess = true;
+    }, 3200);
+  }
 }
